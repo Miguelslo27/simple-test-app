450008a61fa693b4eb9aedeb6db52a4bfea1ca14
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-06-30 11:29:00 -0300

    Split error shows up adding a second test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,6 +5,11 @@ import { App } from "./App";
 describe("App", () => {
   it("should render without error", () => {
     const app = render(<App />);
-    console.log(app.debug());
+    expect(app).toBeTruthy();
+  });
+
+  it("should match snapshot", () => {
+    const app = render(<App />);
+    expect(app).toMatchSnapshot();
   });
 });
